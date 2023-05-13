70f41e75a36ed66eb3530f44586001978ee1cd92
Isael S. Ferreira <isaelsferreira@outlook.com>
2023-05-12 21:58:00 -0300

    Upload de projeto local

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using PrototypeUncafezin.Models;
+using System.Diagnostics;
+using System.IO.Pipelines;
+
+namespace PrototypeUncafezin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Home About Product Dept Basket Checkout Order Login Account Dashboard
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Dept()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            string urlProduct = Request.GetDisplayUrl();
+            ViewBag.urlProduct = urlProduct;
+
+            return View();
+        }
+
+        public IActionResult Basket()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Stores()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,49 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+import jQuery from "jquery";
+
+// Write your JavaScript code.
+jQuery(function () {
+    jQuery('.single-item').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 300
+    });
+
+    jQuery('.carousel').carousel({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 300,
+    });
+
+    jQuery(".list-group-item").click(function () {
+        var shopSrc = jQuery(this).data("src");
+        jQuery("iframe").attr("src", shopSrc);
+    });
+
+    function mostrarDiv(divId) {
+        jQuery('.payment-info').removeClass('active');
+        jQuery('#' + divId).addClass('active');
+    }
+
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
+
+});
